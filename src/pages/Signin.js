08bbf582d61caa7f43bd9679870187f01468f3e4
abdurrahman6489 <abdurrahman489@gmail.com>
2023-06-17 20:35:03 +0530

    pushing the code

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,11 +1,26 @@
 import React from 'react';
-
-
+import {signin} from "../api/auth.js"
+import { useRef,useState } from 'react';
 export const Signin = ()=>{
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+    const [error, setError] = useState(false);
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        const statusPromise = signin({email : emailRef.current.value, password : passwordRef.current.value});
+        const status = statusPromise.then(status => status.statusCode).catch(status => status.statusCode);
+        if(status === 400)
+            setError(true);
+    }
     return(
         <div id="signin-page">
             <h1>Sign in page</h1>
-
+            <form onSubmit={handleSubmit}>
+                <input type="email" id="email" required ref={emailRef} />
+                <input type="password" id="password" required ref={passwordRef}/>
+                <button type="submit">Signin</button>
+            </form>
+            {error && <p class="error-txt">Wrong email or password</p>}
         </div>
     )
 }
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import {signup} from "../api/auth.js"
+import { useRef,useState } from 'react';
 export const Signup = () =>{
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+    const [error, setError] = useState(false);
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        const statusPromise = signup({email : emailRef.current.value, password : passwordRef.current.value});
+        const status = statusPromise.then(status => status.statusCode).catch(status => status.statusCode);
+        if(status === 422)
+            setError(true);
+    }
     return(
         <div id="signin-page">
             <h1>Sign Up page</h1>
-
+            <form onSubmit={handleSubmit}>
+                <input type="email" id="email" required />
+                <input type="password" id="password" required/>
+                <button type="submit">Signup</button>
+            </form>
+            {error && <p class="error-txt">Wrong email or password</p>}
         </div>
     )
 }
